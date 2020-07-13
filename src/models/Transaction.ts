b0a85d3c4e218c6b54a1040a055cefbd0399b3ea
Ernesto Maria <58423237+ernestopmaria@users.noreply.github.com>
2020-07-13 21:35:55 +0100

    add cors

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,4 +1,12 @@
-import {Entity, Column, PrimaryGeneratedColumn, ManyToOne, JoinColumn, CreateDateColumn, UpdateDateColumn} from 'typeorm';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  JoinColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 import Category from './Category';
 
 @Entity('transactions')
@@ -15,18 +23,18 @@ class Transaction {
   @Column('decimal')
   value: number;
 
-  @ManyToOne(()=>Category)
-  @JoinColumn({name:'category_id'})
-  category:Category;
-
   @Column()
   category_id: string;
 
- @CreateDateColumn()
+  @CreateDateColumn()
   created_at: Date;
 
   @UpdateDateColumn()
   updated_at: Date;
+
+  @ManyToOne(() => Category, category => category.transaction, { eager: true })
+  @JoinColumn({ name: 'category_id' })
+  category: Category;
 }
 
 export default Transaction;
--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,31 +11,33 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-
     const transactions = await this.find();
 
-    const {income, outcome} = transactions.reduce((accumulator, transaction)=>{
-      switch (transaction.type){
-        case "income":
-          accumulator.income += Number(transaction.value);
-          break;
+    const { income, outcome } = transactions.reduce(
+      (accumulator, transaction) => {
+        switch (transaction.type) {
+          case 'income':
+            accumulator.income += Number(transaction.value);
+            break;
 
-          case "outcome":
+          case 'outcome':
             accumulator.outcome += Number(transaction.value);
             break;
 
-            default:
-              break;
-      }
-      return accumulator;
-     },{
-       income: 0,
-       outcome:0,
-       total:0,
-     },);
+          default:
+            break;
+        }
+        return accumulator;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
 
-     const total = income- outcome;
-     return { income, outcome, total};
+    const total = income - outcome;
+    return { income, outcome, total };
   }
 }
 
